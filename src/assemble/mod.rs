2504b7cebc65d7ce6c27be394cd83fa0bd29b230
Ashvin Arsakularatne <ashvin.arsakularatne@gmail.com>
2021-07-03 13:21:34 +0530

    feat: add first pass for assembler

--- src/assemble/mod.rs
+++ src/assemble/mod.rs
@@ -1,1 +1,33 @@
-use super::types;
+use std::{collections::HashMap, fs};
+
+use super::types::*;
+
+pub fn run(input_filename: &str, output_filename: &str) -> Result<()> {
+    let raw = fs::read_to_string(input_filename)?;
+
+    // First pass - populate symbol table and isntructions list
+    let (symbol_table, instructions) = extract_labels_and_instructions(raw);
+
+    println!("{:?}", symbol_table);
+    println!("{:?}", instructions);
+
+    Ok(())
+}
+
+fn extract_labels_and_instructions(raw: String) -> (HashMap<String, u32>, Vec<String>) {
+    let mut symbol_table = HashMap::new();
+    let mut instructions = Vec::new();
+
+    let mut address = 0;
+    for line in raw.lines() {
+        let len = line.len();
+        if &line[len - 1..] == ":" {
+            symbol_table.insert(String::from(&line[..len - 1]), address);
+        } else {
+            instructions.push(String::from(line));
+            address += 4;
+        }
+    }
+
+    (symbol_table, instructions)
+}
--- src/bin/assemble.rs
+++ src/bin/assemble.rs
@@ -1,3 +1,23 @@
+use std::{env, process};
+
+use arm11::assemble;
+
 fn main() {
-    print!("{}", "Hello, world!\n");
+    let args: Vec<String> = env::args().collect();
+
+    match args.len() {
+        3 => {
+            let input_filename = &args[1];
+            let output_filename = &args[2];
+            if let Err(e) = assemble::run(input_filename, output_filename) {
+                eprintln!("Error: {}", e);
+                process::exit(1);
+            }
+        }
+
+        _ => {
+            println!("Usage: assemble [assembly] [output]");
+            process::exit(1);
+        }
+    }
 }
